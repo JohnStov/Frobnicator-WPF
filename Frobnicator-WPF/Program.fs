52b849437e0244aa9c14fc972b31774fcd4e4096
John Stovin <john.d.stovin@gmail.com>
2017-10-31 22:28:48 +0000

    Simple user control

--- Frobnicator-WPF/Program.fs
+++ Frobnicator-WPF/Program.fs
@@ -4,37 +4,63 @@ open System
 open Elmish
 open Elmish.WPF
 
-module Types =
+module Counter = 
 
     type Model = { value : int }
+
+    let init () = { value = 0 }
 
     type Message =
         | Increment
 
-module State =
-    open Types
-
-    let init () = { value = 0 }, Cmd.Empty
-
     let update msg model =
         match msg with
-        | Increment -> { model with value = model.value + 1 }, Cmd.Empty
+        | Increment -> { model with value = model.value + 1 }
+
+    let viewBinding : ViewBindings<Model, Message> =
+        [ "Value"       |> Binding.oneWay (fun model -> model.value)
+          "ButtonClick" |> Binding.cmd (fun model msg -> Increment) ]
+
+
+module State =
+    open Counter
+
+    type Model = 
+        { top : Counter.Model; bottom : Counter.Model}
+
+    type Message = 
+        | Top of Counter.Message
+        | Bottom of Counter.Message
+        | Reset
+
+    let init () = 
+        { top = Counter.init () ; bottom = Counter.init ()}
+
+    
+    let update msg model =
+        match msg with
+        | Top msg' -> 
+            let counter' = Counter.update msg' model.top
+            {model with top = counter'}
+        | Bottom msg' -> 
+            let counter' = Counter.update msg' model.bottom
+            {model with bottom = counter'}
+        | Reset ->
+            init () 
 
 module App =
     open State
-    open Types
     open Frobnicator.Views
 
-    let view model dispatch = 
-        [ "Value"       |> Binding.oneWay (fun model -> model.value)
-          "ButtonClick" |> Binding.cmd (fun model msg -> Increment) ]
-            
+    let view _ _ = 
+        ["Reset" |> Binding.cmd (fun model msg -> Reset) 
+         "Top" |> Binding.model (fun model -> model.top) Counter.viewBinding Top
+         "Bottom" |> Binding.model (fun model -> model.bottom) Counter.viewBinding Bottom
+        ]
 
     [<EntryPoint; STAThread>]
     let main argv = 
-        Program.mkProgram init update view
+        Program.mkSimple init update view
         |> Program.withConsoleTrace
         |> Program.runWindow(MainWindow())
-    
-
     
